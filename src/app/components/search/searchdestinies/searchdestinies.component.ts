ecd9f6f9f0edc46277d26b35d0279eaf1ad7f32b
marcocouzin <mcouzin@gmail.com>
2023-06-19 20:10:29 -0300

    Initial commit

---
+++ src/app/components/search/searchdestinies/searchdestinies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchdestinies',
+  templateUrl: './searchdestinies.component.html',
+  styleUrls: ['./searchdestinies.component.css']
+})
+export class SearchdestiniesComponent {
+
+}
